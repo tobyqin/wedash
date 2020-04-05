4511d84fbb0945c49c4cd18128f022660abd4b75
Toby Qin <toby.qin@live.com>
2020-04-05 22:33:57 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,164 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '@/layout'
+
+/**
+ * constantRoutes
+ * a base page that does not have permission requirements
+ * all roles can be accessed
+ */
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
+  },
+  {
+    path: '/volume',
+    component: Layout,
+    redirect: '/volume/home',
+    name: 'Release Volume',
+    meta: { title: 'Release Volume', icon: 'chart' },
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Home', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Release Frequency', icon: 'chart' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: '/volume',
+    component: Layout,
+    redirect: '/volume/home',
+    name: 'Release Volume',
+    meta: { title: 'Success Rate', icon: 'chart' },
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Home', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: '/volume',
+    component: Layout,
+    redirect: '/volume/home',
+    name: 'Release Volume',
+    meta: { title: 'Leading Time', icon: 'chart' },
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Home', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Fix Data', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: 'gsp-devops',
+    component: Layout,
+    children: [
+      {
+        path: 'http://gsp-devops',
+        meta: { title: 'About', icon: 'link' }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  // mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
+})
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
---
+++ src/utils/auth.js
@@ -0,0 +1,15 @@
+import Cookies from 'js-cookie'
+
+const TokenKey = 'gsp_devops_metrics_token'
+
+export function getToken() {
+  return Cookies.get(TokenKey)
+}
+
+export function setToken(token) {
+  return Cookies.set(TokenKey, token)
+}
+
+export function removeToken() {
+  return Cookies.remove(TokenKey)
+}
